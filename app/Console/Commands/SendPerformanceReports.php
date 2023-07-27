0bde0aeff6db4f868abb9dd5f11d81c3c9e61061
Kimuli Arnold <kiflower35@gmail.com>
2023-07-27 02:39:32 -0700

    creaated SendPerformanceMail.php command

---
+++ app/Console/Commands/SendPerformanceReports.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class SendPerformanceReports extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:send-performance-reports';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
