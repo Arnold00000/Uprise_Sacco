1237630e398e8c63ac2930dc0024baba2a57d398
Kimuli Arnold <kiflower35@gmail.com>
2023-07-27 02:54:29 -0700

    report generation logic

--- app/Console/Commands/SendPerformanceReports.php
+++ app/Console/Commands/SendPerformanceReports.php
@@ -3,6 +3,8 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use App\Models\User; // Replace this with the appropriate User model for your project
+use Mail;
 
 class SendPerformanceReports extends Command
 {
@@ -11,20 +13,48 @@ class SendPerformanceReports extends Command
      *
      * @var string
      */
-    protected $signature = 'app:send-performance-reports';
+    //protected $signature = 'app:send-performance-reports';
+    protected $signature = 'report:send';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    //protected $description = 'Command description';
+    protected $description = 'Send performance reports to all active members';
 
     /**
      * Execute the console command.
      */
     public function handle()
     {
-        //
+        $activeMembers = User::where('last_deposit_date', '>=', now()->subMonths(6))->get();
+
+        foreach ($activeMembers as $member) {
+            $reportData = $this->generatePerformanceReport($member);
+
+            Mail::to($member->email)->send(new PerformanceReportMail($reportData));
+        }
+
+        $this->info('Performance reports sent successfully.');
+    }
+
+    private function generatePerformanceReport($member)
+    {
+        // Implement your report generation logic here
+        // You can use the $member object to fetch relevant data and generate the report
+
+        // Example: Generate a simple report
+        $loanProgress = 60; // Replace this with actual loan progress
+        $contributionProgress = 80; // Replace this with actual contribution progress
+
+        $reportData = [
+            'member_name' => $member->name,
+            'loan_progress' => $loanProgress,
+            'contribution_progress' => $contributionProgress,
+        ];
+
+        return $reportData;
     }
 }
