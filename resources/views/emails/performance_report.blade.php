b7375698b06740ea0da35831930018d882ac6fda
Kimuli Arnold <kiflower35@gmail.com>
2023-07-27 07:05:47 -0700

    created performance_report.blade.php view

---
+++ resources/views/emails/performance_report.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Uprise Sacco Performance Report</title>
+</head>
+<body>
+    <h1>Hello {{ $member_name }},</h1>
+    <p>Your loan progress is {{ $loan_progress }}% and your contribution progress is {{ $contribution_progress }}%.</p>
+    <p>Thank you for being an active member of Uprise Sacco.</p>
+    <p>Best regards,</p>
+    <p>The Uprise Sacco Team</p>
+</body>
+</html>
